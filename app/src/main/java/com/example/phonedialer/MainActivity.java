c4201b2bf98c1b8e77101fc960270039aa20ac47
akshaym5147 <akshaymuge888@gmail.com>
2025-08-28 01:57:39 +0530

    removed bottom nav

--- app/src/main/java/com/example/phonedialer/MainActivity.java
+++ app/src/main/java/com/example/phonedialer/MainActivity.java
@@ -31,9 +31,6 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupWithNavController(binding.navView, navController);
-
         checkAndRequestPermissions();
     }
 
